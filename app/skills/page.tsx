930d7e39673d6ab640e94a0f34ee19ab33fa702f
Erick Llerenas <dev.llerenas@gmail.com>
2024-03-01 17:08:45 -0600

    add skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,3 +1,478 @@
 export default function Skills() {
-  return <h1>Skills (WIP)</h1>;
+  return (
+    <div className="container my-24 mx-auto md:px-6">
+      <section className="mb-32 bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6">
+        <div className="flex justify-center">
+          <div className="max-w-[700px] text-center">
+            <h2 className="mb-6 text-3xl font-bold">Flutter</h2>
+            <p className="mb-16 text-neutral-500 dark:text-neutral-300">
+              I love Flutter and have created apps as a freelancer for local
+              clients. My projects include taxi apps, delivery apps, news apps,
+              and some other personal apps to gain experience.
+            </p>
+          </div>
+        </div>
+
+        <div className="grid gap-x-6 md:grid-cols-2 lg:grid-cols-4 xl:gap-x-12">
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Clean code</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I prioritize writting clean and scalable code.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">SOLID</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I love the creating files with single responsability, making
+                  my code easy to read.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Android</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I have created some apps and Android and learnt about its
+                  configuration.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">iOS</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I have created some apps for iOS, similar configuration
+                  process.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Google Play Store</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I have deployed some apps on the Google Developer Console and
+                  learnt some policies.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">App Store</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I have deployed some apps to the App Store, learnt interesting
+                  policies there.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Unit Testing</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I have experience doing unit testing on React, willing to
+                  learn on Flutter.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">E2E Testing</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Same here, have done multiple E2E testing on Cypress, willing
+                  to learn this for Flutter.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mb-32 bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6">
+        <div className="flex justify-center">
+          <div className="max-w-[700px] text-center">
+            <h2 className="mb-6 text-3xl font-bold">React</h2>
+            <p className="mb-16 text-neutral-500 dark:text-neutral-300">
+              I like creating web pages using my expertices on React, I have
+              strong knowledge on it and a very clean code.
+            </p>
+          </div>
+        </div>
+
+        <div className="grid gap-x-6 md:grid-cols-2 lg:grid-cols-4 xl:gap-x-12">
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Clean code</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I prioritize writing clean and scalable code.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">SOLID</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I love creating files with single responsibility, making my
+                  code easier to read.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Custom Hooks</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I love custom hooks to separate the design from the logic of
+                  the app, that's clean!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Almost no useEffects</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Who tought us to use useEffects everywhere? Less is more!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Tailwind</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  For styling our components tailwind brights for the speed!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Modular css</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I also know doing modular css, along with sass and the cx
+                  library for conditional styling.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">Unit Testing</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I have experience doing unit testing with Jest. Buisness logic
+                  is key!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12">
+            <div className="flex">
+              <div className="shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="mr-3 h-5 w-5 text-success"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-1 font-bold">E2E Testing</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  I have done multiple E2E testing on Cypress, testing entires
+                  user flows.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
