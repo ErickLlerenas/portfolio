17097cce1be84a047df02f3eefa59d21b54ea730
Erick Llerenas <dev.llerenas@gmail.com>
2024-03-01 17:59:34 -0600

    make projects responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,44 +2,44 @@ import Image from "next/image";
 import profilePicture from "../public/me.png";
 
 export default function Home() {
-	return (
-		<main className="px-36 pt-44 tracking-wide">
-			<div className="flex flex-row justify-between items-center">
-				<h1 className="text-gray-700 font-bold text-7xl leading-snug">
-					Hey There,
-					<span className="block">{"I'm Erick."}</span>
-				</h1>
-				<div>
-					<p className="text-gray-600 text-lg bg-white p-6 rounded-3xl rounded-bl-md">
-						I do web and mobile
-						<span className="block">development.</span>
-					</p>
-				</div>
-			</div>
+  return (
+    <main className="relative h-screen flex items-end justify-center pb-10 px-4 sm:px-10 md:px-20 lg:px-36">
+      <div className="absolute top-0 left-0 ml-44 mt-44">
+        <h1 className="text-gray-700 font-bold text-3xl md:text-7xl">
+          Hey There,
+          <span className="block">{"I'm Erick."}</span>
+        </h1>
+      </div>
 
-			<div className="flex justify-between">
-				<div className="mt-64 flex flex-row items-center">
-					<span className="text-gray-600 font-extrabold text-6xl">
-						+3
-					</span>
-					<span className="ml-2 text-gray-600 text-xl">
-						Years of experience
-					</span>
-				</div>
+      <div className="absolute top-0 right-0 mr-44 mt-44">
+        <p className="text-gray-600 text-lg bg-white p-4 rounded-3xl rounded-bl-md">
+          I do web and mobile
+          <span className="block">development.</span>
+        </p>
+      </div>
 
-				<div className="mt-64 flex flex-row items-center">
-					<span className="ml-2 text-gray-600 text-xl">
-						Strong <b>SOLID</b> principles
-					</span>
-				</div>
-			</div>
+      <Image
+        src={profilePicture}
+        alt="Erick Llerenas Profile Picture"
+        width={400} // Ajuste fijo de ancho
+        height={400} // Asegurando la relación de aspecto
+        className="w-72 sm:w-64 md:w-96" // Ajustes de tamaño responsivo
+      />
 
-			<Image
-				src={profilePicture}
-				alt="Erick Llerenas Profile Picture"
-				className="absolute bottom-16 left-1/2 transform -translate-x-1/2"
-				style={{ width: 500 }}
-			/>
-		</main>
-	);
+      <div className="absolute bottom-0 left-0 ml-44 mb-52">
+        <span className="text-gray-600 font-extrabold text-3xl md:text-4xl">
+          +3
+        </span>
+        <span className="block text-gray-600 text-lg md:text-xl">
+          Years of experience
+        </span>
+      </div>
+
+      <div className="absolute bottom-0 right-0 mr-44 mb-52">
+        <span className="block text-gray-600 text-lg md:text-xl">
+          Strong <b>SOLID</b> principles
+        </span>
+      </div>
+    </main>
+  );
 }
