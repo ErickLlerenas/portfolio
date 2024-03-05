4e0c39636ae8cef3bfa49c4c8409cccc0c73fea7
Erick Llerenas <dev.llerenas@gmail.com>
2024-03-05 14:28:16 -0600

    remove dark color

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,11 +1,11 @@
 export default function Skills() {
   return (
     <div className="container my-24 mx-auto md:px-6">
-      <section className="mb-32 bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6">
+      <section className="mb-32 bg-white shadow-lg rounded-lg p-6">
         <div className="flex justify-center">
           <div className="max-w-[700px] text-center">
             <h2 className="mb-6 text-3xl font-bold">Flutter</h2>
-            <p className="mb-16 text-neutral-500 dark:text-neutral-300">
+            <p className="mb-16 text-neutral-500">
               I love Flutter and have created apps as a freelancer for local
               clients. My projects include taxi apps, delivery apps, news apps,
               and some other personal apps to gain experience.
@@ -34,7 +34,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Clean code</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I prioritize writting clean and scalable code.
                 </p>
               </div>
@@ -61,7 +61,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">SOLID</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I love the creating files with single responsability, making
                   my code easy to read.
                 </p>
@@ -89,7 +89,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Android</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I have created some apps and Android and learnt about its
                   configuration.
                 </p>
@@ -117,7 +117,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">iOS</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I have created some apps for iOS, similar configuration
                   process.
                 </p>
@@ -145,7 +145,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Google Play Store</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I have deployed some apps on the Google Developer Console and
                   learnt some policies.
                 </p>
@@ -173,7 +173,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">App Store</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I have deployed some apps to the App Store, learnt interesting
                   policies there.
                 </p>
@@ -201,7 +201,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Unit Testing</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I have experience doing unit testing in React and am willing
                   to learn it for Flutter.
                 </p>
@@ -229,7 +229,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">E2E Testing</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   Similarly, I have conducted multiple E2E tests using Cypress
                   and am eager to learn how to apply these testing practices in
                   Flutter.
@@ -240,11 +240,11 @@ export default function Skills() {
         </div>
       </section>
 
-      <section className="mb-32 bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6">
+      <section className="mb-32 bg-white shadow-lg rounded-lg p-6">
         <div className="flex justify-center">
           <div className="max-w-[700px] text-center">
             <h2 className="mb-6 text-3xl font-bold">React</h2>
-            <p className="mb-16 text-neutral-500 dark:text-neutral-300">
+            <p className="mb-16 text-neutral-500">
               I enjoy creating web pages using my expertise in React, where I
               have strong knowledge and maintain very clean code.
             </p>
@@ -272,7 +272,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Clean code</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I prioritize writing clean and scalable code.
                 </p>
               </div>
@@ -299,7 +299,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">SOLID</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I love creating files with single responsibility, making my
                   code easier to read.
                 </p>
@@ -327,7 +327,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Custom Hooks</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I love custom hooks to separate the design from the logic of
                   the app, that&apos;s clean!
                 </p>
@@ -355,7 +355,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Almost no useEffects</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   Who taught us to use useEffects everywhere? Less is more!
                 </p>
               </div>
@@ -382,7 +382,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Tailwind</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   For styling our components, Tailwind shines due to its speed!
                 </p>
               </div>
@@ -409,7 +409,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Modular css</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I am also skilled in modular CSS, utilizing SASS and the cx
                   library for conditional styling.
                 </p>
@@ -437,7 +437,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">Unit Testing</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I have experience in unit testing with Jest. Business logic is
                   key!
                 </p>
@@ -465,7 +465,7 @@ export default function Skills() {
               </div>
               <div className="ml-2 grow">
                 <p className="mb-1 font-bold">E2E Testing</p>
-                <p className="text-neutral-500 dark:text-neutral-300">
+                <p className="text-neutral-500">
                   I have done multiple E2E testing on Cypress, testing entires
                   user flows.
                 </p>
